3dc1beb933c2bdecaf520a0202e67a0483cc3b74
charfweh <affan.ismail2016@gmail.com>
2020-06-19 15:09:50 +0530

    first comiy

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: entryfile.py
---
+++ entryfile.py
@@ -0,0 +1,27 @@
+import os
+import random
+import discord
+client = discord.Client()
+care = 407170811787608064
+@client.event
+async def on_ready():
+    print(
+        f'{client.user} is connected'
+
+    )
+    print(discord.__version__)
+gmood = ["pfft","im in a guud mooooood brooo","hahaha nice nice","this is another res","yall lovely"]
+bmood = ["stfu","where my senpai attt SMH","jeez bunch of dicks in chat lel","bitch fr?"]
+@client.event
+async def on_message(message):
+    if(message.author == client.user): return
+    status= message.guild.get_member(care).status
+    if(status == discord.Status("dnd") or status == discord.Status("idle") or status == discord.Status("online")):
+        res  = random.choice(gmood)
+        await message.channel.send(res)
+    elif(status == discord.Status("offline") or status == discord.Status("invisible")):
+        res = random.choice(bmood)
+        await message.channel.send(res)
+    if(message.content.startswith("hi")):
+        await message.channel.send("Yep ive started")
+client.run(process.env.bot_token)
